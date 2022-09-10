f3894d8f17f423ebdaa3c8b1ab02eb0464eb9218
Saarnaaja <Norilsk.Devil@gmail.com>
2022-09-10 14:19:34 +0700

    Добавлена страница о проекте

--- LifeSpot/Startup.cs
+++ LifeSpot/Startup.cs
@@ -1,7 +1,11 @@
+using System.Collections.Generic;
 using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -24,23 +28,51 @@ namespace LifeSpot
             }
 
             app.UseRouting();
+
+            string footerHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "footer.html"));
+            string sideBarHtml = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "Views", "Shared", "sideBar.html"));
+
+            Dictionary<string, string> replaceItems = new Dictionary<string, string>()
+            {
+                { "<!--SIDEBAR-->", sideBarHtml },
+                { "<!--FOOTER-->", footerHtml },
+            };
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    var viewPath = Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html");
-                    var html = await File.ReadAllTextAsync(viewPath);
-                    await context.Response.WriteAsync(html);
-                });
-                endpoints.MapGet("/Static/CSS/index.css", async context =>
-                {
-                    // по аналогии со страницей Index настроим на нашем сервере путь до страницы со стилями, чтобы браузер знал, откуда их загружать
-                    var cssPath = Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css");
-                    var css = await File.ReadAllTextAsync(cssPath);
-                    await context.Response.WriteAsync(css);
-                });
+                AddMapGet(endpoints, "/", Path.Combine(Directory.GetCurrentDirectory(), "Views", "index.html"), replaceItems);
+                AddMapGet(endpoints, "/testing", Path.Combine(Directory.GetCurrentDirectory(), "Views", "testing.html"), replaceItems);
+                AddMapGet(endpoints, "/about", Path.Combine(Directory.GetCurrentDirectory(), "Views", "about.html"), replaceItems);
+                AddMapGet(endpoints, "/Static/CSS/index.css", Path.Combine(Directory.GetCurrentDirectory(), "Static", "CSS", "index.css"));
+                AddMapGet(endpoints, "/Static/JS/index.js", Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "index.js"));
+                AddMapGet(endpoints, "/Static/JS/testing.js", Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "testing.js"));
+                AddMapGet(endpoints, "/Static/JS/filterContent.js", Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "filterContent.js"));
+                AddMapGet(endpoints, "/Static/JS/about.js", Path.Combine(Directory.GetCurrentDirectory(), "Static", "JS", "about.js"));
             });
 
+        }
+
+        /// <summary>
+        /// Метод для добавления MapGet'ов
+        /// </summary>
+        /// <param name="endpoint"></param>
+        /// <param name="patternt"></param>
+        /// <param name="filePath"></param>
+        /// <param name="replaceItems"></param>
+        private async void AddMapGet(IEndpointRouteBuilder endpoint, string patternt, string filePath, Dictionary<string, string> replaceItems = null)
+        {
+            var html = new StringBuilder(await File.ReadAllTextAsync(filePath));
+            if (replaceItems != null)
+            {
+                foreach (var element in replaceItems)
+                {
+                    html = html.Replace(element.Key, element.Value);
+                }
+            }
+            endpoint.MapGet(patternt, async context =>
+            {
+                await context.Response.WriteAsync(html.ToString());
+            });
         }
     }
 }
---
+++ LifeSpot/Static/JS/index.js
@@ -0,0 +1,57 @@
+﻿let sessionHandler = function handleSession() {
+    // создадим объект Map для хранения сессии
+    let session = new Map();
+    // Сохраним UserAgent
+    session.set("userAgent", window.navigator.userAgent)
+
+
+    // Запросим возраст пользователя и тоже сохраним
+    session.set("age", prompt("Пожалуйста, введите ваш возраст?"))
+
+    // Проверка на возраст и сохранение сессии
+    if (session.get("age") >= 18) {
+        let startDate = new Date().toLocaleString();
+
+        alert("Приветствуем на LifeSpot! " + '\n' + "Текущее время: " + startDate);
+        session.set("startDate", startDate)
+    }
+    else {
+        alert("Наши трансляции не предназначены для лиц моложе 18 лет. Вы будете перенаправлены");
+        window.location.href = "http://www.google.com"
+        a = true + 20 + "name"
+    }
+
+    // Вывод в консоль
+    for (let result of session) {
+        console.log(result)
+    }
+}
+function getReview() {
+    // Создадим объект
+    let review = {}
+
+    // Сохраним свойство имени
+    review["userName"] = prompt("Как вас зовут ?")
+    if (review["userName"] == null) {
+        return
+    }
+
+    // Сохраним текст отзыва
+    review["comment"] = prompt("Напишите свой отзыв")
+    if (review["comment"] == null) {
+        return
+    }
+
+    // Сохраним текущее время
+    review["date"] = new Date().toLocaleString()
+
+    // Добавим на страницу
+    writeReview(review)
+}
+
+const writeReview = review => {
+    document.getElementsByClassName('reviews')[0].innerHTML += '    <div class="review-text">\n' +
+        `<p> <i> <b>${review['userName']}</b>  ${review['date']}</i></p>` +
+        `<p>${review['comment']}</p>` +
+        '</div>';
+}
